a787ca0b1da856613e181f7340783ad975bcf76b
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-02-01 07:51:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+function inIframe() { try {return window.self !== window.top;} catch(e) {return true;} }
 function redirectAfterFunction(el) {
     alert("doing functiony stuff")
     safeRedirect(el.getAttribute('href'));
 }
 function safeRedirect(ref) {
-    var link = document.createElement("a");
-    link.href = ref;
-    link.click();
+    if (inIframe()) {
+        window.location.href = ref;
+        return;
+    }
+    window.parent.postMessage(JSON.stringify({
+        type: 'redirect',
+        data: ref,
+    }), '*');
 }
