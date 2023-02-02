5849a6ec442ecd9a6fa760ea79f69acf516557a4
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-02-02 14:58:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,28 +20,11 @@ document.addEventListener('click', e => {
         safeRedirect(origin.href);
     }
 });
-function setCookie(cname, cvalue, exdays='') {
-    let expires = '';
-    if (exdays != '') {
-        const d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        expires = ";expires="+d.toUTCString();
-    }
-    document.cookie = cname + "=" + cvalue + expires + ";SameSite=Lax;path=/";
+function setCookie(cname, cvalue) {
+    localStorage.setItem(cname, cvalue);
 }
 function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+    return localStorage.getItem(cname);
 }
 function getCookieJSON(x) {
     let out = null;
