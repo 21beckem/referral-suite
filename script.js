aa47747a5938c0c6f1e4b3d8d913836414750016
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-02-08 17:43:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ function fillInContactInfo() {
 }
 async function fillMessageExamples(requestType, folderName, pasteBox) {
     const person = getCookieJSON('linkPages') || null;
-    _('startBlankBtn').setAttribute("onclick", 'window.location.href = "sms:' + person[8] + '"');
+    _('startBlankBtn').setAttribute("onclick", "window.location.replace('sms:" + person[8] + "')" );
     //_('startBlankBtn').onclick = "window.location.href = 'sms:" + person[8] + "'";
 	const reqMssgUrl = 'templates/' + folderName + '/' + encodeURI(requestType) + '.txt';
 	//console.log(reqMssgUrl);
