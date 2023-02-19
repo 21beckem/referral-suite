dae8a1d17c4307e67dee1b7c414ce9def7273e54
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-02-19 17:15:17 +0000

    added Dark Mode!

--- script.js
+++ script.js
@@ -213,9 +213,9 @@ async function fillMessageExamples(requestType, folderName, pasteBox) {
 	for (let i = 0; i < Messages.length; i++) {
         if (Messages[i].match(/{[^}]*}/gm) == null) {
             const this_url = link_beginning + encodeURI(Messages[i]);
-            output += '<div class="w3-panel w3-card-subtle w3-light-grey w3-padding-16"><div class="googleMessage">' + Messages[i] + '</div><a href="' + this_url + '"><div class="useThisTemplateBtn">Use This Template</div></a></div>'
+            output += '<div class="w3-panel w3-card-subtle w3-light-gray w3-padding-16"><div class="googleMessage">' + Messages[i] + '</div><a href="' + this_url + '"><div class="useThisTemplateBtn">Use This Template</div></a></div>'
         } else {
-            output += '<div class="w3-panel w3-card-subtle w3-light-grey w3-padding-16"><div class="googleMessage">' + Messages[i] + '</div><button onclick="sendToCompletionPage(\'' + folderName + '\', this)" class="useThisTemplateBtn">Use This Template</button></div>';
+            output += '<div class="w3-panel w3-card-subtle w3-light-gray w3-padding-16"><div class="googleMessage">' + Messages[i] + '</div><button onclick="sendToCompletionPage(\'' + folderName + '\', this)" class="useThisTemplateBtn">Use This Template</button></div>';
         }
 	}
 	pasteBox.innerHTML = output;
