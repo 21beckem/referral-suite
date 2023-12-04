ac96530eb514fa4f4f42a18ffa995ec2515fadf4
Michael Becker <21beckem8@gmail.com>
2023-12-04 20:22:58 +0100

    tried make biometrics pin-code page. not work yet

--- pin-code.php
+++ pin-code.php
@@ -1,11 +1,14 @@
+<?php
+require_once('require_area.php');
+$missionInfo = json_decode($_COOKIE['missionInfo']);
+?>
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pin Code</title>
+    <title>Pin Login</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <script src="https://kit.fontawesome.com/0bddc0a0f7.js" crossorigin="anonymous"></script>
     <link href='https://fonts.googleapis.com/css?family=Advent Pro' rel='stylesheet'>
@@ -14,8 +17,7 @@
     <script src="https://21beckem.github.io/WebPal/WebPal.js"></script>
     <script src="jsalert.js"></script>
     <script src="everyPageFunctions.php"></script>
-    <script src="script.js" dont-redirect="true" no-fox="true"></script>
-    <script src="fox.js"></script>
+    <link rel="icon" type="image/png" href="/referral-suite/logo.png" />
     <meta name="mobile-web-app-capable" content="yes">
     <link rel="manifest" href="manifest.webmanifest">
     <meta name="theme-color" content="#462c6a">
@@ -82,8 +84,46 @@
     </center>
 
     <script>
+const randomStringFromServer = 'hi';
+async function attemptBio() {
+    const publicKeyCredentialCreationOptions = {
+        challenge: Uint8Array.from(
+            randomStringFromServer, c => c.charCodeAt(0)),
+        rp: {
+            name: "Referral Suite",
+            id: window.location.hostname,
+        },
+        user: {
+            id: Uint8Array.from(
+                "UZSL85T9AFC", c => c.charCodeAt(0)),
+            name: "<?php echo($__TEAM->name . ' | ' . $missionInfo->name) ?>",
+            displayName: "<?php echo($__TEAM->name . ' | ' . $missionInfo->name) ?>",
+        },
+        pubKeyCredParams: [{alg: -7, type: "public-key"}],
+        authenticatorSelection: {
+            authenticatorAttachment: "cross-platform",
+        },
+        timeout: 60000,
+        attestation: "direct"
+    };
+    await navigator.credentials.get({publicKey: {
+        challenge: Uint8Array.from(
+            'hi', c => c.charCodeAt(0)),
+        allowCredentials: [{
+            id: Uint8Array.from(
+                'hi', c => c.charCodeAt(0)),
+            type: 'public-key',
+            transports: ['usb', 'ble', 'nfc'],
+        }],
+        timeout: 60000,
+    }});
+    // const credential = await navigator.credentials.create({
+    //     publicKey: publicKeyCredentialCreationOptions
+    // });
+    console.log(credential.id);
+}
+attemptBio();
 
-SYNC_getConfig();
 let currentPin = '';
 
 function addNumber(num) {
@@ -97,15 +137,17 @@ function addNumber(num) {
         currentPin = currentPin.slice(4);
     }
     if (currentPin.length == 4) {
-        if (currentPin == CONFIG['overall settings']['login pin']) {
-            sessionStorage.setItem("logged_in", "1");
-            safeRedirect('index.html');
-        } else {
-            _('pin-code').classList.add('shake');
-            setTimeout(function(el) {
-                _('pin-code').classList.remove('shake');
-            }, 300);
-        }
+        // test pin, probably by having the server check
+
+        // if (currentPin == CONFIG['General']['pin code']) {
+        //     sessionStorage.setItem("logged_in", "1");
+        //     safeRedirect('index.html');
+        // } else {
+        //     _('pin-code').classList.add('shake');
+        //     setTimeout(function(el) {
+        //         _('pin-code').classList.remove('shake');
+        //     }, 300);
+        // }
     }
     _('pin-code').innerHTML = '<tr>' + Array(currentPin.length).fill('<td>•</td>').join('') + Array(4 - currentPin.length).fill('<td></td>').join('') + '</tr>';
 }
