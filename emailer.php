d0e99dc6f5c49ea3bd756bc12cff2cb23273e7ad
Michael Becker <21beckem8@gmail.com>
2023-12-16 21:16:26 +0100

    Emailer page looks better now. All UI nearly done

--- emailer.php
+++ emailer.php
@@ -20,6 +20,41 @@ require_once('require_area.php');
   <meta name="mobile-web-app-capable" content="yes">
   <link rel="manifest" href="manifest.webmanifest">
   <meta name="theme-color" content="#462c6a">
+  <style>
+#emailBodyInput {
+  width: calc(100% - 20px);
+  background-color: var(--white);
+  padding: 5px;
+  border-radius: 5px;
+  min-height: 33px;
+  max-height: 325px;
+  overflow-y: scroll;
+}
+#emailSendBtn {
+  width: 33px;
+  height: 33px;
+  background-color: transparent;
+  border: none;
+  line-height: normal;
+  font-size: 25px;
+  transform: translate(-10px,-2px);
+}
+#allMessages .message.us {
+  float: right;
+  box-shadow: 0 0 13px -9px red inset;
+}
+#allMessages .message {
+  box-shadow: 0 0 13px -9px var(--mainColor) inset;
+  float:left;
+  border: 1px solid rgba(118, 118, 118, 0.3);
+  background-color: rgba(239, 239, 239, 0.3);
+  width: calc(100% - 20px);
+  padding: 10px;
+  margin-bottom: 15px;
+  min-height: 45px;
+  border-radius: 5px;
+}
+  </style>
 </head>
 
 <body>
@@ -28,32 +63,46 @@ require_once('require_area.php');
     <div onclick="safeRedirect('contact_info.php')" class="w3-container w3-cell w3-xlarge w3-left-align">
       <i class="fa-solid fa-angle-left w3-text-white" style="position: relative;"></i>
     </div>
-    <div class="w3-container w3-padding-16">
+    <div class="w3-container w3-padding-16" style="text-align:center">
       <div class="contact_info" id="contactname"></div>
     </div>
+    <div class="w3-container w3-cell w3-xlarge w3-right-align"></div>
   </div>
   <div style="height: 80px;"></div>
 
-  <!-- List of items -->
+  <!-- All messages -->
   <div class="w3-container">
-    <div id="unclaimedlist"></div>
+    <div id="allMessages">
+      <div class="message us">Jag heter {Your Name}. Jag är representant med webbsidan VandraiTro.se Jag fick din information för att du svarade på en annons att du vill få en besök av några av våra</div>
+      <div class="message">Ja</div>
+      <div class="message us">Jag heter {Your Name}. Jag är representant med webbsidan VandraiTro.se Jag fick din information för att du svarade på en annons att du vill få en besök av några av våra</div>
+      <div class="message">Faktist: nej.</div>
+      <div class="message us">BRUH!!!!!!! Jag heter {Your Name}. Jag är representant med webbsidan VandraiTro.se Jag fick din information för att du svarade på en annons att du vill få en besök av några av våra</div>
+      <div class="message">Eller... Faktist...</div>
+      <div class="message us">Jag heter {Your Name}. Jag är representant med webbsidan VandraiTro.se Jag fick din information för att du svarade på en annons att du vill få en besök av några av våra</div>
+    </div>
   </div>
 
   <!-- Bottom Nav Bar -->
-  <div style="height: 80px;"></div>
-  <div style="width:100%; display: flex; align-items:flex-start; padding: 10px" id="BottomNavBar">
+  <div id="bottomNavSpacer" style="height: 61px;"></div>
+  <div style="width:100%; display: flex; align-items:flex-end; padding-left:10px; min-height: unset;" id="BottomNavBar">
     <div style="flex:1">
-      <textarea style="width:calc(100% - 20px)" name="" id=""></textarea>
+      <div contenteditable id="emailBodyInput"></div>
     </div>
     <div>
-      <button><i class="fa-solid fa-paper-plane"></i></button>
+      <button id="emailSendBtn"><i class="fa-solid fa-paper-plane"></i></button>
     </div>
   </div>
   <script>
 
 const person = idToReferral(getCookie('linkPages'));
 _('contactname').innerHTML = person[TableColumns['first name']] + ' ' + person[TableColumns['last name']];
+const emailBodyInput = _('emailBodyInput');
 
+emailBodyInput.addEventListener("input", (e) => {
+  //console.log(e);
+  _('bottomNavSpacer').style.height = (_('BottomNavBar').scrollHeight + 5) + 'px';
+}, false);
   </script>
 </body>
 </html>
